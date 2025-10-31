6aab3d7696d617119b619930b69c358207f04bf6
Alonso <gomez.alonso07@gmail.com>
2025-10-31 11:45:01 +0100

    Practica 13 de Python

---
+++ Ejercicio6/app/estadisticas.py
@@ -0,0 +1,10 @@
+import statistics
+
+def calcular_media(lista):
+    return statistics.mean(lista)
+
+def calcular_mediana(lista):
+    return statistics.median(lista)
+
+def calcular_moda(lista):
+    return statistics.mode(lista)
---
+++ Ejercicio6/paquete/subpaquete/division.py
@@ -0,0 +1,4 @@
+def division(a, b):
+    if b == 0:
+        raise ValueError("No se puede dividir entre cero.")
+    return a / b
